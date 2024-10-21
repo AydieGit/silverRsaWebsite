017663fcafbf97bcc92a3c7a00710a5df67f1356
AydieGit <163208966+AydieGit@users.noreply.github.com>
2024-10-21 11:22:54 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,100 +1,151 @@
-import Image from "next/image";
+'use client'
+import Head from 'next/head';
+import Image from 'next/image';
+import living from "./public/rowyMainRoom.jpg";
+import bath from "./public/rowyBathRoom.jpg";
+import kitchen from "./public/rowyKitchen.jpg";
+import sleeping from "./public/rowySleepingRoom.jpg";
+import sleeping2 from "./public/rowySleepingRoom2.jpg";
+import mapa from "./public/mapa.jpg";
+import pl from "./public/flagaPl.jpg";
+import usa from "./public/flagaUSA.jpg"
+import { useState } from 'react';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [language, setLanguage] = useState('pl'); // Ustawienie języka domyślnego na polski
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+  // Funkcja zmieniająca język na angielski
+  const switchToEnglish = () => {
+    setLanguage('en');
+  };
+
+  // Funkcja zmieniająca język na polski
+  const switchToPolish = () => {
+    setLanguage('pl');
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>Apartamenty RSA Silver</title>
+        <meta name="description" content="Komfortowy apartament w Rowach, blisko Słowińskiego Parku Narodowego" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* Przycisk zmiany języka */}
+      <div className="language-switcher">
+        {language === 'pl' ? (
+
+            <Image 
+            id="lang-en" className="language-button" onClick={switchToEnglish}
+            width={75}
+            height={75}
+            alt="usa flag"
+            src={usa}
+            /> 
+        ) : (
+
+            <Image 
+            id="lang-en" className="language-button" onClick={switchToPolish}
+            width={75}
+            height={75}
+            alt="usa flag"
+            src={pl}
+            /> 
+        )}
+      </div>
+
+      {/* Główna sekcja z flexboxem */}
+      <main className="main-content">
+        <section className="left-content">
+          <section className="text-center py-12">
+            <h1>{language === 'pl' ? 'RSA Silver - Twój idealny apartament w Rowach' : 'RSA Silver - Your perfect apartment in Rowy'}</h1>
+            <h2>{language === 'pl' ? 'Plażowa 5, 76-212 Rowy, Polska' : 'Plażowa 5, 76-212 Rowy, Poland'}</h2>
+            <h2>{language === 'pl' ? 'Zadzwoń 509 786 989' : 'Call me  +48 509 786 989'}</h2>
+            <p className="text-wrap">
+              {language === 'pl' 
+                ? 'Oferujemy komfortowy, ciekawie zaaranżowany i bogato wyposażony apartament położony w sąsiedztwie Słowińskiego Narodowego Parku, idealny dla rodzin, osób aktywnych i seniorów.'
+                : 'We offer a comfortable, creatively arranged, and well-equipped apartment located near the Słowiński National Park, ideal for families, active people and seniors.'
+              }
+            </p>
+
+            <div className="mt-8 flex flex-wrap justify-center items-center image-gallery">
+              <Image src={living} width={500} height={250} alt="Main Room" className="gallery-image" />
+              <Image src={bath} width={500} height={250} alt="Bath Room" className="gallery-image" />
+              <Image src={kitchen} width={500} height={250} alt="Kitchen" className="gallery-image" />
+              <Image src={sleeping2} width={500} height={250} alt="Sleeping Room" className="gallery-image" />
+              <Image src={sleeping} width={500} height={250} alt="Sleeping Room" className="gallery-image" />
+              <Image src={mapa} width={500} height={250} alt="Mapa" className="gallery-image" />
+            </div>
+          </section>
+
+          {/* Atrakcje w okolicy */}
+          <section className="mt-12 text-center">
+            <h2>{language === 'pl' ? 'Atrakcje w okolicy' : 'Attractions in the area'}</h2>
+            <p>
+              {language === 'pl' 
+                ? `"RSA Silver" oferuje komfortowy, ciekawie zaaranżowany i bogato wyposażony apartament położony w sąsiedztwie Słowińskiego Narodowego Parku...`
+                : `"RSA Silver" offers a comfortable, creatively arranged, and well-equipped apartment located near the Słowiński National Park...`}
+            </p>
+          </section>
+
+          {/* Lokalizacja */}
+          <section className="mt-12 text-center">
+            <h2>{language === 'pl' ? 'Lokalizacja' : 'Location'}</h2>
+            <p>
+              {language === 'pl' 
+                ? 'Apartament RSA Silver położony jest zaledwie 100m od centrum i szerokiej, piaszczystej plaży obiekt zaopatrzony w:'
+                : 'The RSA Silver apartment is located just 100 meters from the center and a wide, sandy beach. The property is equipped with:'}
+            </p>
+            <ul className="text-left mx-auto max-w-xl">
+              <li>{language === 'pl' ? '✔ Bezpłatne WiFi' : '✔ Free WiFi'}</li>
+              <li>{language === 'pl' ? '✔ Prywatny parking' : '✔ Private parking'}</li>
+              <li>{language === 'pl' ? '✔ Telewizor z płaskim ekranem' : '✔ Flat-screen TV'}</li>
+              <li>{language === 'pl' ? '✔ W pełni wyposażony aneks kuchenny' : '✔ Fully equipped kitchenette'}</li>
+              <li>{language === 'pl' ? '✔ Łazienka z prysznicem' : '✔ Bathroom with shower'}</li>
+              <li>{language === 'pl' ? '✔ Balkon z widokiem na miasto' : '✔ Balcony with city view'}</li>
+            </ul>
+            <p>
+            {language === 'pl' 
+                ? `Odległość ważnych miejsc od obiektu: Plaża dla psów Dębina-Ustka – 1,3 km, Słowiński Park Narodowy – 33 km. Najbliższe lotnisko, Lotnisko Gdańsk-Rębiechowo, znajduje się 141 km od obiektu RSA Silver.`
+                : `Distance from important places: Dog Beach Dębina-Ustka – 1.3 km, Słowiński National Park – 33 km. The nearest airport, Gdańsk-Rębiechowo Airport, is located 141 km from the RSA Silver property.`}
+            </p>
+          </section>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+
+      {/* Przycisk do rezerwacji */}
+      <section className="mt-12 text-center contact">
+        <a 
+          id="link" 
+          href="https://www.booking.com/hotel/pl/rsa-silver.pl.html" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className="button"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+          {language === 'pl' ? 'Skontaktuj się z nami' : 'Contact us'}
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      </section>
+
+      {/* Mapa */}
+      <section className="map-section mt-12">
+        <h2 className="text-center">{language === 'pl' ? 'Lokalizacja RSA Silver' : 'RSA Silver Location'}</h2>
+        <div className="map-container">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2254.7890125976497!2d17.05339431625784!3d54.662297480268955!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46fead39fb295bfb%3A0x4c24d76167b7e6e5!2sPla%C5%BCowa%205%2C%2076-212%20Rowy%2C%20Polska!5e0!3m2!1spl!2spl!4v1697276269744!5m2!1spl!2spl"
+            width="100%"
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            title="Mapa lokalizacji RSA Silver"
+          ></iframe>
+        </div>
+      </section>
+
+      <footer className="text-center">
+        <p>&copy; 2024 RSA Silver. {language === 'pl' ? 'Wszelkie prawa zastrzeżone.' : 'All rights reserved.'}</p>
       </footer>
     </div>
   );
